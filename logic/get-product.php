7add2954f8dc23ce3561d7046b710ac457ae5bfd
radithya027 <rizkyradithya027@gmail.com@gmail.com>
2025-01-25 13:29:45 +0700

    [RR] fetch product bar

--- logic/get-product.php
+++ logic/get-product.php
@@ -1,20 +1,33 @@
 <?php
 require_once '../services/db-connection.php';
 
+// Create a new XML document
+$xml = new SimpleXMLElement('<?xml version="1.0" encoding="UTF-8"?><contact/>');
+
 // Execute SQL query to fetch all columns
 $sql = "SELECT * FROM products";
 $result = $conn->query($sql);
 
-// Display all results
+// Add products to XML
 if ($result->num_rows > 0) {
     while ($row = $result->fetch_assoc()) {
-        echo "ID: " . $row["id"] . " - Name: " . $row["name"] . " - Price: " . $row["price"] . 
-             " - Image: " . $row["image"] . " - Description: " . $row["description"] . 
-             " - Size: " . $row["size"] . "<br>";
+        $product = $xml->addChild('product');
+        $product->addChild('id', $row['id']);
+        $product->addChild('name', htmlspecialchars($row['name']));
+        $product->addChild('price', $row['price']);
+        
+        $image = $product->addChild('image');
+        $image->addChild('url', htmlspecialchars($row['image']));
+        
+        $product->addChild('description', htmlspecialchars($row['description']));
+        $product->addChild('size', $row['size']);
+        $product->addChild('title', htmlspecialchars($row['name']));
     }
-} else {
-    echo "No products found";
 }
+
+// Output XML
+header('Content-Type: application/xml');
+echo $xml->asXML();
 
 $conn->close();
 ?>
